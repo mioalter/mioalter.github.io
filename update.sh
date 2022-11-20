3b1ba7fa9c7b76962ea41859071771b4671df50d
Mio Ilan Alter <mioalter@gmail.com>
2022-11-19 22:12:40 -0500

    update script

--- update.sh
+++ update.sh
@@ -1,4 +1,9 @@
 git checkout develop -- _site/
-cp -a _site/ .
+cp _site/about.html about.html
+cp _site/archive.html archive.html
+cp _site/contact.html contact.html
+cp _site/index.html index.html
+cp -r _site/css/ .
+cp -r _site/posts/ .
 rm -rf _site/
 git rm -r _site/
