8cf0de66e9aa2781f4b912fb3cf11a740cfc5efc
Mio Ilan Alter <mioalter@gmail.com>
2023-12-12 12:44:48 -0500

    cleanup

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ cp _site/archive.html archive.html
 cp _site/contact.html contact.html
 cp _site/index.html index.html
 cp -r _site/css/ css/
-cp -r _site/css/ images/
+cp -r _site/images/ images/
 cp -r _site/posts/ posts/
 rm -rf _site/
 git rm -r _site/
